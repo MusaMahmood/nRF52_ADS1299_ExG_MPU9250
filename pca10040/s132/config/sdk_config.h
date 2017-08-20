c7f48e489e4f1610a552498b3ccb2af47ea2f81a
AD\mmahmood31 <musasmahmood@gmail.com>
2017-08-20 14:43:34 -0400

    All optimizations applied for power saving.

--- pca10040/s132/config/sdk_config.h
+++ pca10040/s132/config/sdk_config.h
@@ -7,7 +7,7 @@
 // <h> nRF_BLE 
 
 #ifndef LEDS_ENABLE
-#define LEDS_ENABLE 1
+#define LEDS_ENABLE 0
 #endif
 
 //==========================================================
@@ -3076,7 +3076,7 @@
 #if  APP_TIMER_ENABLED
 
 #ifndef APP_TIMER_SAMPLING
-#define APP_TIMER_SAMPLING 1
+#define APP_TIMER_SAMPLING 0
 #endif
 
 // <o> APP_TIMER_CONFIG_RTC_FREQUENCY  - Configure RTC prescaler.
@@ -3717,7 +3717,7 @@
 // <e> NRF_LOG_ENABLED - nrf_log - Logging
 //==========================================================
 #ifndef NRF_LOG_ENABLED
-#define NRF_LOG_ENABLED 1 //Check NRF_LOG_BACKEND.. .RTT
+#define NRF_LOG_ENABLED 0 //Check NRF_LOG_BACKEND.. .RTT
 #endif
 #if NRF_LOG_ENABLED
 // <e> NRF_LOG_USES_COLORS - If enabled then ANSI escape code for colors is prefixed to every string
@@ -3919,7 +3919,7 @@
 // <e> NRF_LOG_BACKEND_SERIAL_USES_RTT - If enabled data is printed using RTT
 //==========================================================
 #ifndef NRF_LOG_BACKEND_SERIAL_USES_RTT
-#define NRF_LOG_BACKEND_SERIAL_USES_RTT 1
+#define NRF_LOG_BACKEND_SERIAL_USES_RTT 0
 #endif
 #if  NRF_LOG_BACKEND_SERIAL_USES_RTT
 // <o> NRF_LOG_BACKEND_RTT_OUTPUT_BUFFER_SIZE - RTT output buffer size. 
